23cc1397bedd9d890af5e6dba6319016abe428c4
cjoan <chadjoan@gmail.com>
2016-06-16 02:22:20 -0400

    Don't attempt to version-control the executable.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+mkdir -p bin
 dmd src/*.d \
 		src/c/*.d src/c/libssh/*.d src/c/net/*.d src/c/openssl/*.d \
 		src/ssh/*.d \
-	-ofremotely -gc -debug -Isrc \
+	-ofbin/remotely -gc -debug -Isrc \
 	-L-lssh -L-Llibressl/prefix -L-lssl -L-lcrypto
